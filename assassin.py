a49ce84e101cd3fc7f3be7eea0616f8cbdcaf430
fspider <codemissile@hotmail.com>
2020-01-21 12:41:10 +0800

    add image upload

--- assassin.py
+++ assassin.py
@@ -1,30 +1,48 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 from face_compare import compare
-
+import os
 
 app = Flask(__name__)
 
-mainfrm = "<form action='compare' method='GET'>\
-            Video URL:<br>\
-            <input type='text' name='source_face' value='sample/source.jpg'/>\
-            <input type='text' name='target_face' value='sample/target.jpg'/>\
-            <input type='submit' value='Submit'>\
-        </form>"
+
 
 
 @app.route('/')
 def index():
-    return mainfrm
+    return render_template("main.html", result = "")
 
-@app.route('/compare', methods = ['GET'])
+@app.route('/compare', methods = ['GET', 'POST'])
 def processVideo():
-    # read source and target image from GET and set default Value 
-    sourceURL = request.args.get('source_face', 'sample/source.jpg')
-    targetURL = request.args.get('target_face', 'sample/target.jpg')
-    result = compare(sourceURL, targetURL)
-    return mainfrm + "<br>" + "Src = " + str(sourceURL) + "<br>Tgt = " + str(targetURL) + "<br> RESULT = " + str(result)
+    if request.method == "GET":
+        return render_template("main.html", result = "")
+    if not request.files:
+        return render_template("main.html", result = "No input!")
+
+    try:
+        image1 = request.files["image1"]
+        image1_path = os.path.join(app.config["IMAGE_UPLOADS"], image1.filename)
+        image1.save(image1_path)
+        print("Image1 saved", image1_path)
+
+        image2 = request.files["image2"]
+        image2_path = os.path.join(app.config["IMAGE_UPLOADS"], image2.filename)
+        image2.save(image2_path)
+        print("Image2 saved", image2_path)
+
+
+        # # read source and target image from GET and set default Value 
+        # sourceURL = request.args.get('source_face', 'sample/source.jpg')
+        # targetURL = request.args.get('target_face', 'sample/target.jpg')
+        # return mainfrm + "<br>" + "Src = " + str(sourceURL) + "<br>Tgt = " + str(targetURL) + "<br> RESULT = " + str(result)
+
+        result = compare(image1_path, image2_path)
+        return render_template("main.html", result = result)
+    except Exception as e:
+        print(e)
+        return render_template("main.html", result = "ERROR")
 
 
 if __name__ == '__main__':
-#    app.run(debug=True)
-    app.run(host='0.0.0.0', threaded=True)
+#    app.run()
+    app.config["IMAGE_UPLOADS"] = "sample"
+    app.run(host='192.168.0.114', threaded=True)
--- face_compare.py
+++ face_compare.py
@@ -3,7 +3,17 @@ import face_recognition
 def compare(sourceURL, targetURL):
     srcImg = face_recognition.load_image_file(sourceURL)
     tgtImg = face_recognition.load_image_file(targetURL)
-    src_enc = face_recognition.face_encodings(srcImg)[0]
-    tar_enc = face_recognition.face_encodings(tgtImg)[0]
-    results = face_recognition.compare_faces([src_enc], tar_enc)
-    return results[0]
+    src_encs = face_recognition.face_encodings(srcImg) 
+    tar_encs = face_recognition.face_encodings(tgtImg)
+    if len(src_encs) < 1 or len(tar_encs) < 1:
+        return False
+    src_enc = src_encs[0]
+    tar_enc = tar_encs[0]
+    distance = face_recognition.face_distance([src_enc], tar_enc)
+
+    print(distance)
+
+    if distance < 0.5:
+        return True
+    return False
+
